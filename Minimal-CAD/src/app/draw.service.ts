724ad18f2ecf25da002e80f01e1744e3e1be0f06
noaloehrer <noa.loehrer@optimatik.ch>
2025-07-31 15:50:53 +0200

    style attachments

--- Minimal-CAD/src/app/draw.service.ts
+++ Minimal-CAD/src/app/draw.service.ts
@@ -1,32 +1,49 @@
 import { Injectable, model } from '@angular/core';
-import { FormObject } from './interfaces';
+import { FormObject, FreeObject } from './interfaces';
 import modelData from './models/model-data.json' assert { type: "json" };
 
 @Injectable({
   providedIn: 'root'
 })
 export class Draw {
+  public showGrid = true;
+  public showSideBar = true;
+  public sideBarContent: FormObject | FreeObject | null = null;
+
   loadObjects(): FormObject[] {
     return modelData as FormObject[];
   }
 
+  getShowGrid(): boolean {
+    return this.showGrid;
+  }
+
+  setShowGrid(value: boolean): void {
+    this.showGrid = value;
+  }
+
+  getShowSideBar(): { show: boolean; content: FormObject | FreeObject | null } {
+    return { show: this.showSideBar, content: this.sideBarContent };
+  }
+
+  setShowSideBar(value: boolean, object: FormObject | FreeObject): void {
+    this.showSideBar = value;
+    this.sideBarContent = object;
+  }
+
   line() {
-    // Logic to create a line
+    // Comming soon: Line objects will be implemented later
   }
 
   rectangle() {
-    // Logic to create a rectangle
-  }
-
-  triangle() {
-    // Logic to create a triangle
+    
   }
 
   circle() {
-    // Logic to create a circle
+    
   }
 
   extrusion() {
-    // Logic to create an extrusion
+    // Coming soon: Extrusion objects will be implemented later
   }
 }
