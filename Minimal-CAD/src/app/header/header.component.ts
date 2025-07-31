8424f8c46785e68f10586a6598b1243d67a08086
noaloehrer <noa.loehrer@optimatik.ch>
2025-07-31 08:34:38 +0200

    header initialization

--- Minimal-CAD/src/app/header/header.component.ts
+++ Minimal-CAD/src/app/header/header.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatIconModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  ngOnInit() {
+    // Initialization logic here
+  }
 
+  save() {
+    // Save logic here
+  }
+
+  upload() {
+    // Upload logic here
+  }
 }
