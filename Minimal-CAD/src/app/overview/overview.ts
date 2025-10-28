4dad96f275f24817e2d188399f535a86bca157b5
noalohrer125 <noalohrer125@gmail.com>
2025-10-28 12:58:18 +0100

    implement overview-page frontend

--- Minimal-CAD/src/app/overview/overview.ts
+++ Minimal-CAD/src/app/overview/overview.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { Project } from '../interfaces';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-overview',
@@ -10,5 +12,25 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './overview.css'
 })
 export class Overview {
+  public projects: Project[] = [
+    {
+      id: '1',
+      name: 'Sample Project',
+      licenceKey: 'public',
+      ownerEmail: 'you@example.com',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      objectIds: []
+    }
+  ];
 
+  constructor(private router: Router) {}
+
+  openProject(projectId: string) {
+    this.router.navigate(['/project', projectId]);
+  }
+
+  openProjectActions() {
+    throw new Error('Method not implemented.');
+  }
 }
