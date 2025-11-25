0ba2852bc767e52574934e604db9c540d113e709
noalohrer125 <noalohrer125@gmail.com>
2025-11-25 08:42:28 +0100

    add prod-environment

---
+++ Minimal-CAD/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+// Production environment configuration
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyATrilv_ZIZKmpb2-Ak209PdtA1ndfC_7E",
+    authDomain: "minimalcad-1a6dd.firebaseapp.com",
+    projectId: "minimalcad-1a6dd",
+    storageBucket: "minimalcad-1a6dd.firebasestorage.app",
+    messagingSenderId: "199921853686",
+    appId: "1:199921853686:web:3cab7b19921a9dd83978d7",
+    measurementId: "G-NYS2569YXM"
+  }
+};
---
+++ Minimal-CAD/src/environments/environment.ts
@@ -0,0 +1,13 @@
+// Development environment configuration
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyATrilv_ZIZKmpb2-Ak209PdtA1ndfC_7E",
+    authDomain: "minimalcad-1a6dd.firebaseapp.com",
+    projectId: "minimalcad-1a6dd",
+    storageBucket: "minimalcad-1a6dd.firebasestorage.app",
+    messagingSenderId: "199921853686",
+    appId: "1:199921853686:web:835de982aa7d3ce83978d7",
+    measurementId: "G-L5K9ZYGYEK"
+  }
+};
