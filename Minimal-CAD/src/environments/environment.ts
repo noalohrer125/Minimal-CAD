899c385d725b5217a05a183ca5348b39de667b02
noalohrer125 <noalohrer125@gmail.com>
2025-12-15 14:43:44 +0100

    change dev-environment

--- Minimal-CAD/src/environments/environment.ts
+++ Minimal-CAD/src/environments/environment.ts
@@ -2,12 +2,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyATrilv_ZIZKmpb2-Ak209PdtA1ndfC_7E",
-    authDomain: "minimalcad-1a6dd.firebaseapp.com",
-    projectId: "minimalcad-1a6dd",
-    storageBucket: "minimalcad-1a6dd.firebasestorage.app",
-    messagingSenderId: "199921853686",
-    appId: "1:199921853686:web:835de982aa7d3ce83978d7",
-    measurementId: "G-L5K9ZYGYEK"
+  apiKey: "AIzaSyCApXsFkAy3maDnsiz2rA5qN6Ch8aln36E",
+  authDomain: "minimalcad-dev.firebaseapp.com",
+  projectId: "minimalcad-dev",
+  storageBucket: "minimalcad-dev.firebasestorage.app",
+  messagingSenderId: "299674671888",
+  appId: "1:299674671888:web:d611497c61918edb9b2395",
+  measurementId: "G-PC87KSDCV7"
   }
 };
