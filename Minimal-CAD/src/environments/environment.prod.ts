ccc81f78d069cfe4be8f49fea3475be47e3eb871
noalohrer125 <noalohrer125@gmail.com>
2025-12-16 09:03:21 +0100

    change url for prod

--- Minimal-CAD/src/environments/environment.prod.ts
+++ Minimal-CAD/src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
     projectId: "minimalcad-1a6dd",
     storageBucket: "minimalcad-1a6dd.firebasestorage.app",
     messagingSenderId: "199921853686",
-    appId: "1:199921853686:web:3cab7b19921a9dd83978d7",
-    measurementId: "G-NYS2569YXM"
+    appId: "1:199921853686:web:c059624ed600e41a3978d7",
+    measurementId: "G-7F5R90NSWC"
   }
 };
